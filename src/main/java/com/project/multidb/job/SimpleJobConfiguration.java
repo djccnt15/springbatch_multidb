9b4fe543bab18baf180f636e3249ca79b2b835cc
Jay.D <djccnt15@gmail.com>
2024-10-30 23:45:38 +0900

    minor update for identifying sample job

--- src/main/java/com/project/multidb/job/SimpleJobConfiguration.java
+++ src/main/java/com/project/multidb/job/SimpleJobConfiguration.java
@@ -27,7 +27,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 @Slf4j
-@Batch
+// @Batch
 @RequiredArgsConstructor
 public class SimpleJobConfiguration {
     
