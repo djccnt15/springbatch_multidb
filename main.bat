32b079cc9b998dbe303b201ecaa9757d28bb3f65
Jay.D <djccnt15@gmail.com>
2024-10-31 00:18:51 +0900

    create main.bat for run batch app at windoww server update build.gradle to exclude application.yaml when build jar file

---
+++ main.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: Set the path to your Java installation (replace this with your actual path)
+set JAVA_PATH=C:\programming\java\jdk-17.0.0.1\bin\java.exe
+set XMS=128M
+set XMX=256M
+
+:: Get the current date and format it
+for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value') do set DATETIME=%%i
+set BASE_DATE=%DATETIME:~0,4%-%DATETIME:~4,2%-%DATETIME:~6,2%
+
+:: Set the classpath (replace this with the path to your jar and dependencies)
+set CLASSPATH=C:\projects\springbatch_multidb\build\libs\multidb.jar
+set CONFIG_PATH=C:\projects\springbatch_multidb\src\main\resources\application.yaml
+
+:: Run the Spring Batch application
+%JAVA_PATH% -jar "%CLASSPATH%" baseDate=%BASE_DATE% -Xms%XMS% -Xmx%XMX% --spring.config.location=%CONFIG_PATH%
